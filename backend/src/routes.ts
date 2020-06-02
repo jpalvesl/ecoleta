4aa4439c777da4bfd9776aaba68cb23bf05ddfe0
João Lima <jpalvesl101@gmail.com>
2020-06-02 13:46:55 +0000

    criando rota que lista os itens

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import knex from './database/connection';
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  
-  return res.json({ ok: true })
+routes.get('/items', async(req, res) => {
+  const items = await knex('items').select('*')
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `https://3333-beeeb93e-7bba-4a92-b5a9-b1f6f1e7b7c9.ws-us02.gitpod.io/uploads/${item.image}`
+    }
+  })
+
+
+  return res.json(serializedItems)
 })
 
 export default routes;
