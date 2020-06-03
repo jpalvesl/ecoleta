97443ebe11ca924623650be1ac336ed0b9093104
João Lima <jpalvesl101@gmail.com>
2020-06-03 17:02:28 +0000

    conectando com a api e buscando imagens dos itens

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import api from '../../services/api';
 
 import { Container, Header, Form, Field, FieldGroup, FieldCheck, ItemsGrid } from './styles';
 
 import logo from '../../assets/logo.svg';
 
+interface Item {
+  id: number; 
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+
+  useEffect(() => {
+    api.get('/items').then(response => {
+      setItems(response.data)
+    })
+  }, [])
+
   return (
     <Container>
       <Header>
@@ -95,29 +112,11 @@ const CreatePoint = () => {
           </legend>
 
           <ItemsGrid>
-            <li>
-              <img src="https://3333-beeeb93e-7bba-4a92-b5a9-b1f6f1e7b7c9.ws-eu01.gitpod.io/uploads/oleo.svg" alt=""/>
+            {items.map(item => (
+            <li key={item.id}>
+              <img src={item.image_url} alt={item.title}/>
             </li>
-
-            <li>
-              <img src="" alt=""/>
-            </li>
-
-            <li>
-              <img src="" alt=""/>
-            </li>
-
-            <li>
-              <img src="" alt=""/>
-            </li>
-
-            <li>
-              <img src="" alt=""/>
-            </li>
-
-            <li>
-              <img src="" alt=""/>
-            </li>
+            ))}
 
           </ItemsGrid>
         </fieldset>
---
+++ web/src/services/api.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://3333-beeeb93e-7bba-4a92-b5a9-b1f6f1e7b7c9.ws-eu01.gitpod.io',
+})
+
+export default api;
