d3e266b66c108789dc23f511fa6055cd00356f4f
João Lima <jpalvesl101@gmail.com>
2020-06-04 13:36:27 +0000

    Iniciando estilizaçao da home

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+
+import { 
+  Container,
+  Logo,
+  Main,
+  Title,
+  Description,
+
+  Footer,
+  Button,
+  ButtonIcon,
+  ButtonText,
+
+
+} from './styles';
+
+const logo = require('../../assets/logo.png');
+const background = require('../../assets/home-background.png');
+
+const Home = () => {
+  return (
+    <Container 
+      source={background}
+      imageStyle={{ width: 274, height: 368  }}   
+    >
+      <Main>
+        <Logo source={logo} />
+        <Title>Seu marketplace de coleta de residuos</Title>
+        <Description>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Description>
+      </Main>
+
+      <Footer>
+        <Button onPress={() => {}}>
+          <ButtonIcon>
+            <Feather name="arrow-right" color="#fff" size={24} />
+          </ButtonIcon>
+
+          <ButtonText>Entrar</ButtonText>
+        </Button>
+      </Footer>
+    </Container>
+  )
+}
+
+export default Home;
